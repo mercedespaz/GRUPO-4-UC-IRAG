1ea0cf33e1487e38479a55a4262039e591a58059
mercedespaz <pazmercedes.mp@gmail.com>
2025-09-15 12:22:33 -0300

    Observaciones gráfico internaciones

--- Graficos/agrupado.R
+++ Graficos/agrupado.R
@@ -1,42 +1,39 @@
 #librerias 
 library(readr)
-library(readr)
 library(tidyverse)
 library(highcharter)
 
-#carga de la base de datos agrupados
-UC_IRAG_Carga_Agrupada_Mendoza_Notti_HOSPITAL_HUMBERTO_J_NOTTI_ <- read_csv("scripts/UC IRAG - Carga Agrupada-Mendoza-Notti - HOSPITAL HUMBERTO J. NOTTI.-.csv")
+#Carga de la base de datos agrupados
+UC_IRAG_Carga_Agrupada <- read_csv("UC IRAG - Carga Agrupada-Mendoza-Notti - HOSPITAL HUMBERTO J. NOTTI.-.csv")
+
+#Elimino primeraa fila que no contiene datos
+UC_IRAG_Carga_Agrupada <-UC_IRAG_Carga_Agrupada[-1,]
 
 #unique(UC_IRAG_Carga_Agrupada_Mendoza_Notti_HOSPITAL_HUMBERTO_J_NOTTI_$NOMBREEVENTOAGRP)
 
-#crear el data frame de datos agrupados segun nuestras variables de interés
+#Crear el data frame de datos agrupados segun nuestras variables de interés
 
-dataagrupado <- UC_IRAG_Carga_Agrupada_Mendoza_Notti_HOSPITAL_HUMBERTO_J_NOTTI_ %>%
-  # Filtrar columnas y filas de interés
-  select(ANIO, SEMANA, NOMBREEVENTOAGRP, 
-         `0 a 2 m`, `3 a 5 m`, `6 a 11 m`,
+evento<-c("Pacientes internados por todas las causas", "Casos de IRAG entre los internados","Casos de IRAG extendida entre los internados")
+
+dataagrupado <- UC_IRAG_Carga_Agrupada %>%
+  # Filtrar columnas de interés
+  select(ANIO, SEMANA, NOMBREEVENTOAGRP, `0 a 2 m`, `3 a 5 m`, `6 a 11 m`,
          `12 a 23 m`, `2 a 4 años`, `5 a 9 años`, `10 a 14 años`) %>%
+  # Filtrar filas 
   filter(
-    NOMBREEVENTOAGRP %in% c(
-      "Pacientes internados por todas las causas",
-      "Casos de IRAG entre los internados",
-      "Casos de IRAG extendida entre los internados"
-      
-    ) &
-      (
-        (ANIO == 2024 & SEMANA >= 23) |
-          (ANIO == 2025 & SEMANA <= 34)
-      )
-  ) %>%
+    NOMBREEVENTOAGRP %in% evento &
+      ((ANIO == 2024 & SEMANA >= 23) |
+       (ANIO == 2025 & SEMANA <= 34))) %>%
   # Crear etiqueta de semana y año
-  mutate(sepi_label = paste0(ANIO, " - SE ", SEMANA))
+  mutate(
+    sepi_label = paste(ANIO, "-", SEMANA),
+    sepi_label = factor(sepi_label, levels = unique(paste(ANIO, "-", SEMANA))))
 
-# se estructura a formato largo: pasar columnas de edad a una variable "grupo_etario"
+# Se estructura a formato largo: pasar columnas de edad a una variable "grupo_etario"
 # y guardar los valores en "casos_totales"
 
-dataagrupado<-dataagrupado %>%
-  pivot_longer(4:10, names_to = "grupo_etario",
-                values_to = "casos_totales" )
+dataagrupado<-dataagrupado %>% pivot_longer(4:10, names_to = "grupo_etario",
+                                            values_to = "casos_totales" )
 
 #se transforma la variable casos totales a numérica
 dataagrupado$casos_totales<-as.numeric(dataagrupado$casos_totales)
@@ -48,29 +45,34 @@ dataagrupado<-dataagrupado %>%
             .groups = ("drop"))
 
 # se estructura a formato ancho,separando categorías de NOMBREEVENTOAGRP en columnas
-dataagrupado<-dataagrupado %>%
-  pivot_wider(names_from = NOMBREEVENTOAGRP,
-               values_from = casosinternados)
+dataagrupado<-dataagrupado %>% pivot_wider(names_from = NOMBREEVENTOAGRP,
+                               values_from = casosinternados)
 
 #se corre este comando para calcular la suma de IRAG e IRAGe
 dataagrupado<- dataagrupado %>%
-  mutate(IRAG_totales =
-           (`Casos de IRAG entre los internados`+`Casos de IRAG extendida entre los internados`)) 
+  mutate(IRAG_totales =(`Casos de IRAG entre los internados`+`Casos de IRAG extendida entre los internados`),
+         Otras_internaciones =(`Pacientes internados por todas las causas`-`IRAG_totales`))
 
 # se calcula el porcentaje de IRAG total (IRAG + IRAGe) sobre internaciones totales
-dataagrupado<- dataagrupado %>%
-  mutate(pct_IRAG = (`IRAG_totales` / 
-                       `Pacientes internados por todas las causas`) * 100)
-
+dataagrupado<-dataagrupado %>% mutate(pct_irag = 
+  round(`IRAG_totales` / `Pacientes internados por todas las causas` * 100,1))
 
 #realizar grafico en highcharter: % IRAGtotal /internaciones totales por SE
 Gráficoagrupado <- highchart() %>%
   hc_xAxis(categories = dataagrupado$sepi_label,
-           title = list(text = "Semana Epidemiológica")) %>%
+           title = list(text = "Semana epidemiológica")) %>%
   hc_yAxis_multiples(
-    list(title = list(text = "Número de casos"), opposite = FALSE),
-    list(title = list(text = "% IRAG e IRAGe sobre internados"), 
-         opposite = TRUE, max = 100)
+    list(title = list(text = "Número de internaciones"), 
+         opposite = FALSE, 
+         min = 0, 
+         gridLineWidth = 1),  
+    list(title = list(text = "% Internaciones por IRAG e IRAGe"), 
+         opposite = TRUE, 
+         min = 0, 
+         max = 100,
+         ceiling = 100,
+         endOnTick = FALSE,
+         gridLineWidth = 0)   
   ) %>%
   hc_plotOptions(
     column = list(
@@ -78,33 +80,32 @@ Gráficoagrupado <- highchart() %>%
       dataLabels = list(enabled = FALSE)
     ),
     line = list(
-      dataLabels = list(enabled = TRUE, format = "{y}%")
+      dataLabels = list(enabled = FALSE)
     )
   ) %>%
   # Serie IRAG
-  hc_add_series(name = "Casos de IRAG e IRAGe",
+  hc_add_series(name = "Internaciones por IRAG e IRAGe",
                 type = "column",
-                data = dataagrupado$`IRAG_totales`,
+                data = dataagrupado$IRAG_totales,
                 color = "#a6bddb",
                 yAxis = 0,
                 stack = "internados") %>%
-  # Serie otros internados (se calcula automáticamente = total - IRAG)
-  hc_add_series(name = "Internaciones totales",
+  hc_add_series(name = "Internaciones por otras causas",
                 type = "column",
-                data = dataagrupado$`Pacientes internados por todas las causas` - dataagrupado$`IRAG_totales`,
+                data = dataagrupado$Otras_internaciones,
                 color = "#1f77b4",
                 yAxis = 0,
                 stack = "internados") %>%
   # Línea de porcentaje
-  hc_add_series(name = "% IRAG e IRAGe",
+  hc_add_series(name = "% Internaciones por IRAG e IRAGe",
                 type = "line",
-                data = round(dataagrupado$pct_IRAG, 1),
+                data = dataagrupado$pct_irag,
                 color = "#ff8000",
-                yAxis = 1,
-                tooltip = list(valueSuffix = "%"),
-                dataLabels = list(enabled = TRUE, format = "{y}%")) %>%
+                yAxis = 1)%>%
+  hc_exporting(enabled= TRUE) %>%
   hc_title(text = "Porcentaje de IRAG e IRAGe sobre el total de internaciones hospitalarias. 
   Desde SE23 2024-SE34 2025.\n Hospital Dr. Humberto J. Notti, Mendoza.")
+
 
 Gráficoagrupado
 
