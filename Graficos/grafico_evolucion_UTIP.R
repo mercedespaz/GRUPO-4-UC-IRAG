a6fa266a55a35102e92d6fd455563900bcd6528b
Mara Russo <mrusso@ANLIS2>
2025-09-22 15:41:41 -0300

    Porcentajes UTIP IRAG e IRAGe

--- Graficos/grafico_evolucion_UTIP.R
+++ Graficos/grafico_evolucion_UTIP.R
@@ -18,6 +18,33 @@ data_grafico_utip <- data %>% filter(CLASIFICACION_MANUAL != "Caso invalidado po
                                      summarise(casos = n(), .groups = "drop")
 
 
+# Extraemos los valores de casos
+valor_IRAG_extendida <- data_grafico_utip %>% filter(CUIDADO_INTENSIVO == "SI") %>%
+  filter(CLASIFICACION_MANUAL == "IRAG extendida") %>%
+  pull(casos)
+
+valor_IRAG <- data_grafico_utip %>%
+  filter(CLASIFICACION_MANUAL == "Infección respiratoria aguda grave (IRAG)") %>% filter(CUIDADO_INTENSIVO == "SI") %>%
+  pull(casos)
+
+# Calculamos los porcentajes
+
+#CASOS DE IRAG E IRAGE POR SE
+
+data_filtrada <- data %>% select(CLASIFICACION_MANUAL,SEPI_MIN_INTERNACION,ANIO_MIN_INTERNACION) %>%
+  filter(CLASIFICACION_MANUAL != "Caso invalidado por epidemiología") %>% 
+  group_by(CLASIFICACION_MANUAL,SEPI_MIN_INTERNACION, ANIO_MIN_INTERNACION) %>%
+  summarise(casos_semana = n(),.groups = "drop") %>% 
+  arrange(ANIO_MIN_INTERNACION) %>% 
+  pivot_wider(names_from = CLASIFICACION_MANUAL,values_from = casos_semana)
+
+casos_IRAGe <- sum(data_filtrada$`IRAG extendida`, na.rm = TRUE)
+casos_IRAG <- sum(data_filtrada$`Infección respiratoria aguda grave (IRAG)`, na.rm = TRUE)
+
+porc_IRAG_UTI <- round((valor_IRAG / casos_IRAG) * 100, 1)
+porc_IRAGe_UTI <- round((valor_IRAG_extendida / casos_IRAGe) * 100, 1)
+
+
 #Categorias tipo IRAG
 categorias <- c("Infección respiratoria aguda grave (IRAG)", 
                 "IRAG extendida")
