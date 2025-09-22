28e3567b5eaca619502b1f56beccf2db43eb0d7f
mercedespaz <pazmercedes.mp@gmail.com>
2025-09-22 16:25:27 -0300

    Observación nombre establecimiento

--- Establecimiento.R
+++ Establecimiento.R
@@ -3,8 +3,10 @@
 # ===============================
 establecimiento <- unique(data$ESTABLECIMIENTO_INTERNACION)
 
+print(establecimiento)
+
 establecimiento <- str_to_title(establecimiento)
 
-establecimiento <- str_replace_all(establecimiento, "_", " ")
+establecimiento <- str_replace_all(establecimiento, "-", " ")
 
 
