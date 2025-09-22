0897a0fb678e99844285a77d951b629a78a1d645
Mara Russo <mrusso@ANLIS2>
2025-09-22 11:22:08 -0300

    Hicimos un R del establecimiento.

---
+++ Establecimiento.R
@@ -0,0 +1,6 @@
+# ===============================
+# Valor único de ESTABLECIMIENTO_INTERNACION
+# ===============================
+establecimiento <- unique(data$ESTABLECIMIENTO_INTERNACION)
+
+unique(data$ESTABLECIMIENTO_INTERNACION)
