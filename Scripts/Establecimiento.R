f070a810a7c6deae9d74d87010340cbbb3a8fea7
mercedespaz <pazmercedes.mp@gmail.com>
2025-09-24 11:12:23 -0300

    Establecimiento

--- Scripts/Establecimiento.R
+++ Scripts/Establecimiento.R
@@ -1,8 +1,4 @@
 # ===============================
 # Valor único de ESTABLECIMIENTO_INTERNACION
 # ===============================
-establecimiento <- unique(data$ESTABLECIMIENTO_INTERNACION)
-
-establecimiento <- str_to_title(establecimiento)
-
-establecimiento <- str_replace_all(establecimiento, "-", " ")
+establecimiento <- "Hospital Humberto J. Notti"
